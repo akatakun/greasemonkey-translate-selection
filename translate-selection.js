f83915f7274db2b35de61a4e8ac140331fb48f93
akata in cygwin <akata.onen@gmail.com>
2015-06-25 23:01:00 +0900

    commit

---
+++ translate-selection.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name        TranslateSelection
+// @namespace   Akata
+// @include     *
+// @version     1
+// @grant       GM_xmlhttpRequest
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// ==/UserScript==
+function KeyCheck(e) {
+  // When press 'e' key, translate selection with Weblio.
+  if(e.keyCode === 69) {
+    var selection = window.getSelection();
+    GM_xmlhttpRequest( {
+        method: 'POST',
+        url: 'http://translate.weblio.jp/',
+        data: 'lp=' + encodeURIComponent('EJ') + '&originalText=' + encodeURIComponent(selection),
+        headers: {
+          'User-Agent': 'Mozilla/5.0',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        onload: function(response) {
+          var dom = $.parseHTML(response.responseText);
+          console.log('* `' + selection + '`: ' + $(dom).find('.transExpB li').map( function() {
+                return $(this).text();
+          } ).get().join(', '));
+          $(dom).find('.translatedTextAreaLn span').each(function() { console.log( '- ' + $(this).text()); } );
+        }
+    } );
+  }
+  // When press 'w' key, search selection with Wikipedia.
+  if(e.keyCode === 87) {
+    var selection = window.getSelection();
+    var url = 'https://ja.wikipedia.org/w/index.php?search=' +  encodeURIComponent(selection);
+    console.log(url);
+    GM_xmlhttpRequest( {
+        method: 'GET',
+        url: url,
+        headers: {
+          'User-Agent': 'Mozilla/5.0',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        onload: function(response) {
+          var dom = $.parseHTML(response.responseText);
+          console.log($(dom).find('.mw-body-content p:first').text());
+        }
+    } );
+  }
+}
+window.addEventListener('keydown', KeyCheck, true);
